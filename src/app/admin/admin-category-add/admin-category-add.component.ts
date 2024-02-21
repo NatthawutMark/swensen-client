2fdf002b6a291f9a9d91f9f329431a33ff4fea1b
MarkAengVoyy <natthawut09870@gmail.com>
2024-02-21 23:12:35 +0700

    add

---
+++ src/app/admin/admin-category-add/admin-category-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-category-add',
+  templateUrl: './admin-category-add.component.html',
+  styleUrls: ['./admin-category-add.component.scss']
+})
+export class AdminCategoryAddComponent {
+
+}
